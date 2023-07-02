28f55f3075ad0e58171d284cdbd08db531be4a38
a-maged <maged.dev@gmail.com>
2023-07-02 06:57:19 +0300

    add validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,18 @@
 import { BaseCrudController } from 'src/BaseCrudController';
 import { User } from './user.model';
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { DataSource } from 'typeorm';
+import { UserDTO } from './user.dto';
 
 @Controller('/users')
 export class UsersController extends BaseCrudController<User> {
   constructor(dataSource: DataSource) {
     super(User, dataSource);
+  }
+
+  /* Override to validate using a DTO */
+  @Post()
+  create(data: UserDTO): Promise<User> {
+    return super.create(data);
   }
 }
